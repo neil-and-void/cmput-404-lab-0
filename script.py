1a36b73c47c2d603e71490ec54e1765d5f86f2a8
Neilzon <neilcviloria@gmail.com>
2023-01-09 17:11:34 -0700

    add url for script

--- script.py
+++ script.py
@@ -2,6 +2,6 @@ import requests
 
 print(requests.__version__) # 2.28.1
 
-res = requests.get('')
+res = requests.get('https://raw.githubusercontent.com/neilZon/cmput-404-lab-0/main/script.py')
 
 print(res.text)
